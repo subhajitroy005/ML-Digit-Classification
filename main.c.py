3194804450623b3bd56afc50c1b707edc0c5f680
Subhajit Roy <subhajitroy005@gmail.com>
2026-07-25 01:35:42 +0530

    Dataload class implemented

--- main.c.py
+++ main.c.py
@@ -1,35 +1,31 @@
-from pathlib import Path as dir
+import matplotlib.pyplot as plot
 from PIL import Image
-import numpy as np
 
-
-num_of_folders = 10
-
-# Structure array
-dataset_structure = []
-
-IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png"]
+from preprocessing.data_load import Dalaloader
 
 
 
-for i in range(1, num_of_folders + 1):
-    # get the folder names
-    folder_path = dir(f"./dataset/{i}")
 
-    # Directory wise images put inside a structure in lables
-    if not (folder_path.is_dir()):
-        continue
 
-    #images array container
-    images_list = []
+def main():
+    """ Load the data """
+    print("--- Step 1: Data loading ---")
+    Loader = Dalaloader(data_path="./dataset")
+    collected_data = Loader.collect_from_folder(num_folders=9)
+    print(f"Loaded {len(collected_data)} images")
 
-    # Loop through files and put inside the structure
-    for files in folder_path.iterdir():
-        if files.suffix.lower() in IMAGE_EXTENSIONS:
-            img = Image.open(files)
-            img_arr = np.array(img)
-            images_list.append(img_arr)
+    image_arr = collected_data[0]["images"][0]
+    label = collected_data[0]["label"][0]
 
-    #Now append to the strucre
-    dataset_structure.append( {"label": "{i}", "images": images_list } )
+    image = Image.fromarray(image_arr)
 
+    print(collected_data[0]["images"][0].shape)
+
+    plot.imshow(image)
+    plot.title(label)
+    plot.axis('off')
+    plot.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing/data_load.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import numpy as np
+from PIL import Image
+from contourpy.util import data
+
+
+class Dalaloader:
+    """ Class responsible for data load from dataset """
+
+    def __init__(self, data_path="./dataset", extensions=".png"):
+        self.base_path = Path(data_path)
+        if extensions is None:
+            self.extensions = {".png"}
+        else:
+            self.extensions = extensions
+
+    def collect_from_folder(self, num_folders):
+        """ Loop through dataset folders and collect images  """
+
+        dataset_struct = []
+
+        for i in range(1, num_folders+1):
+            folder_path = self.base_path/str(i)
+
+            if not(folder_path.exists()):
+                print(f"Folder not found {folder_path}")
+                continue
+
+        images_list = []
+
+        for file_path in folder_path.iterdir():
+            if file_path.suffix.lower() in self.extensions:
+                try:
+                    img = Image.open(file_path)
+                    img_arr = np.array(img)
+                    images_list.append(img_arr)
+                except Exception as e:
+                    print(f"Error loading {file_path}:{e}")
+
+            dataset_struct.append({"label":str(i) , "images":images_list})
+
+        return dataset_struct
